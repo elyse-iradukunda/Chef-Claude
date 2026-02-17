f4fe2933cc51f6668469104dc11f1b92aa820aa7
Your Name <ellycreativity8@gmail.com>
2026-02-17 18:01:38 +0200

    here I have added form on my project

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,17 @@
-export default function Main(){
-
-    return(
-        <main>
-            <form action="">
-                <input aria-label="Add Ingredient"
-                placeholder="e.g. oregano"
-                type="text" />
-                <button>+ add ingredient</button>
-            </form>
-        </main>
-    )
+export default function Main() {
+  return (
+    <main className="flex items-center justify-center pt-20 bg-gray-50">
+      <form className="flex gap-3 bg-white  rounded-xl shadow-md">
+        <input
+          aria-label="Add Ingredient"
+          placeholder="e.g. oregano"
+          type="text"
+          className="border border-gray-300 rounded-lg px-4 py-2 w-100"
+        />
+        <button className="bg-black text-white px-5 py-2 rounded-lg font-semibold">
+          + Add Ingredient
+        </button>
+      </form>
+    </main>
+  );
 }

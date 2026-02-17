08e1b5bc78575bed7837288596db8b4a79d8431b
Your Name <ellycreativity8@gmail.com>
2026-02-17 17:51:57 +0200

    here I have added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Header from "./components/Header"
+
 function App() {
   return (
-    <h1 className="text-4xl font-bold text-blue-500">
-      Tailwind is Working!
-    </h1>
+    <>
+     <Header/>
+    </>
   )
 }
 
-export default App
+export default App 
---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+export default function Main(){
+
+    return(
+        <main>
+            <form action="">
+                <input aria-label="Add Ingredient"
+                placeholder="e.g. oregano"
+                type="text" />
+                <button>+ add ingredient</button>
+            </form>
+        </main>
+    )
+}

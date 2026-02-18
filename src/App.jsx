f4210f25b59c965519e406681d6abd139916fe66
Your Name <ellycreativity8@gmail.com>
2026-02-18 18:23:22 +0200

    here is changes on cheif claude in my project that I started yesterday

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Header from "./components/Header"
-import Main from "./components/Main"
+import Form from "./components/Form"
 
 function App() {
   return (
     <>
      <Header/>
-     <Main/>
+     <Form/>
     </>
   )
 }
